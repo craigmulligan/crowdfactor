c56a93cb57482480b85de37fa52d273b9fc23f8d
Craig Mulligan <git@craigmulligan.com>
2022-10-31 16:05:05 -0600

    basics

--- main.py
+++ main.py
@@ -1,54 +1,182 @@
+import os
+from typing import Optional, cast, Dict
 import json
-import requests
+import sys
+import shutil
+import glob
+from urllib.parse import parse_qs
+from collections import Counter
+
+# Third-party
+import av
 from bs4 import BeautifulSoup
-from dataclasses import dataclass
 import ffmpeg
+import requests
+from roboflow import Roboflow
+
+ROBOFLOW_API_KEY = os.environ.get("ROBOFLOW_API_KEY")
 
 
-@dataclass
 class Camera:
+    id: str
     title: str
     url: str
+    frame_rate: int
+    duration: int
 
+    def __init__(self, id, title, url, frame_rate=60, duration=60):
+        self.id = id
+        self.title = title
+        self.url = url
+        self.frame_rate = frame_rate
+        self.duration = duration
 
-def get_video(camera: Camera):
-    # Given cam url download the stream to a file.
-    ffmpeg.input(camera.url).trim(duration=10).output("output.mp4").run()
+    @property
+    def data_dir(self):
+        return f"data/{self.id}"
 
+    @property
+    def video_file_name(self):
+        return f"{self.data_dir}/output.mp4"
 
-def get_info(url):
-    """
-    Gets the camera stream url via spot URL.
-    """
-    # Headers are needed otherwise surfline tries to bounce you.
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
-    }
-    page = requests.get(url, allow_redirects=True, headers=headers)
-    soup = BeautifulSoup(page.text, "html.parser")
-    content = soup.find("script", id="__NEXT_DATA__")
-    assert content
-    data = json.loads(content.text)
-    spot_data = data["props"]["pageProps"]["ssrReduxState"]["spot"]["report"]["data"][
-        "spot"
-    ]
-    print(f"found: {spot_data['name']}")
-    print(f"with cameras:")
+    def workspace(self):
+        if os.path.isdir(self.data_dir):
+            shutil.rmtree(self.data_dir)
+            # if the demo_folder2 directory is
+            # not present then create it.
 
-    # TODO improve error handling.
-    if not len(spot_data["cameras"]):
-        raise Exception("No Cam")
+        os.makedirs(self.data_dir)
 
-    # Always just use the first one for now.
-    camera = spot_data["cameras"][0]
+    def write_video(self):
+        # Given cam url download the stream to a file.
+        if os.path.exists(self.video_file_name):
+            os.remove(self.video_file_name)
 
-    if camera["status"]["isDown"]:
-        raise Exception("Cam is down.")
+        ffmpeg.input(self.url).trim(duration=self.duration).output(
+            self.video_file_name
+        ).run()
 
-    return Camera(camera["title"], camera["streamUrl"])
+    def write_images(self):
+        container = av.open(self.video_file_name)
+
+        for i, frame in enumerate(container.decode(video=0)):
+            if i % self.frame_rate == 0:
+                frame.to_image().save(f"{self.data_dir}/frame-%04d.jpg" % frame.index)
+
+    def analyze(self):
+        rf = Roboflow(api_key=ROBOFLOW_API_KEY)
+        project = rf.workspace().project("the-local")
+        model = project.version(2).model
+        assert model
+        classes = ["people", "surfer-riding", "surfer-lying", "surfer-paddling"]
+        print("predicting...")
+        counters = []
+
+        for filename in glob.glob(f"{self.data_dir}/*.jpg"):
+            # checking if it is a file
+            predictions = model.predict(filename, confidence=50, overlap=50)
+            if predictions is None:
+                continue
+
+            counter = Counter()
+
+            for prediction in predictions:
+                # For whatever reason typing thinks prediction is int.
+                prediction = cast(Dict, prediction)
+
+                if prediction["class"] in classes:
+                    counter.update([prediction["class"]])
+
+            print("ran prediction", counter)
+
+            counters.append(counter)
+
+        return counters
+
+    def crowd_counter(self, counters):
+        """
+        Because people can be obscured by frames we want to take the highest count
+        over the period.
+        """
+        totals = [c.total() for c in counters]
+        totals.sort(reverse=True)
+        return totals[0]
+
+    @staticmethod
+    def get_camera_id(url: str) -> Optional[str]:
+        qs = parse_qs(url)
+        cam_id = qs.get("camId")
+
+        if cam_id:
+            return cam_id[0]
+
+        return None
+
+    @staticmethod
+    def get_from_url(url: str):
+        """
+        Gets the camera stream url via spot URL.
+        """
+        # Headers are needed otherwise surfline tries to bounce you.
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+        }
+        page = requests.get(url, allow_redirects=True, headers=headers)
+        soup = BeautifulSoup(page.text, "html.parser")
+        content = soup.find("script", id="__NEXT_DATA__")
+        assert content
+        data = json.loads(content.text)
+        spot_data = data["props"]["pageProps"]["ssrReduxState"]["spot"]["report"][
+            "data"
+        ]["spot"]
+        print(f"found: {spot_data['name']}")
+
+        camera_id = Camera.get_camera_id(url)
+
+        # TODO improve error handling.
+        if not len(spot_data["cameras"]):
+            raise Exception("No Cam")
+
+        camera = spot_data["cameras"][0]
+
+        # Always just use the first one for now.
+        if camera_id is not None:
+            cameras = [
+                camera for camera in spot_data["cameras"] if camera["_id"] == camera_id
+            ]
+            if len(cameras):
+                raise Exception(f"Could not find camera {camera_id}")
+
+            camera = cameras[0]
+
+        if camera["status"]["isDown"]:
+            raise Exception("Cam is down.")
+
+        return Camera(camera["_id"], camera["title"], camera["streamUrl"])
 
 
 if __name__ == "__main__":
-    url = "https://www.surfline.com/surf-report/77th-st-rockaways/584204214e65fad6a7709d0a?camId=583498c9e411dc743a5d5288"
-    camera = get_info(url)
-    get_video(camera)
+    if len(sys.argv) < 2:
+        raise Exception(
+            "a <url> is required eg: https://www.surfline.com/surf-report/venice-breakwater/590927576a2e4300134fbed8"
+        )
+
+    if ROBOFLOW_API_KEY is None:
+        raise Exception(
+            "Missing ROBOFLOW_API_KEY - make sure to set it as an environment variable"
+        )
+
+    url = sys.argv[1]
+
+    camera = Camera.get_from_url(url)
+    # setup workspace
+    # This cleans up any old files from previous runs.
+    camera.workspace()
+
+    # # # First save the 10s of live feed to video file
+    camera.write_video()
+    # # # # Them transform video to images
+    camera.write_images()
+    # Then analyze images.
+    counters = camera.analyze()
+    print("crowd count: ", camera.crowd_counter(counters))

961c01ac108ad77c7aba698fa31a05c8515e0530
Craig Mulligan <git@craigmulligan.com>
2022-11-03 15:39:32 -0600

    Touch ups

--- start.sh
+++
@@ -1,6 +0,0 @@
-# Run worker every ~10ish mins
-while true
-do 
-    python worker.py
-    sleep 600
-done
--- worker.py
+++ worker.py
@@ -1,59 +1,18 @@
-import os
-import sys
-from lib.camera import Camera, NightTimeError, CameraDownError
-from lib.db import DB
+import logging
 from lib.app import app
-from lib.utils import DATETIME_FORMAT
-from datetime import datetime, timezone
+from lib import worker
+from time import sleep
+from lib.camera import CameraDownError, NightTimeError
 
 if __name__ == "__main__":
-    # Running in flask app
-    # context ensures we cleanup db connections
-    # etc.
     with app.app_context():
-        ROBOFLOW_API_KEY = os.environ.get("ROBOFLOW_API_KEY")
-        SURFLINE_SPOT_ID = os.environ.get("SURFLINE_SPOT_ID")
+        while True:
+            try:
+                worker.run()
+            except (CameraDownError, NightTimeError) as e:
+                # Valid errors
+                logging.warning(e)
+            except Exception as e:
+                logging.exception("Unexpected error running worker")
 
-        if ROBOFLOW_API_KEY is None:
-            raise Exception(
-                "Missing ROBOFLOW_API_KEY - make sure to set it as an environment variable"
-            )
-
-        if SURFLINE_SPOT_ID is None:
-            raise Exception(
-                "Missing SURFLINE_SPOT_ID - make sure to set it as an environment variable"
-            )
-
-        db = DB.get_db()
-        # ensure db schema
-        db.setup()
-
-        try:
-            camera = Camera.get(SURFLINE_SPOT_ID, ROBOFLOW_API_KEY)
-        except (CameraDownError, NightTimeError) as e:
-            # Valid errors
-            print(e)
-            sys.exit(0)
-
-        # setup workspace
-        # This cleans up any old files from previous runs.
-        camera.workspace()
-
-        # # # First save the 10s of live feed to video file
-        camera.write_video()
-        # # # # Them transform video to images
-        camera.write_images()
-        # Then analyze images.
-        counters = camera.analyze()
-        crowd_count = camera.crowd_counter(counters)
-
-        now = datetime.now().replace(tzinfo=timezone.utc)
-        db.insert(
-            crowd_count,
-            camera.surf_rating,
-            camera.spot_id,
-            now.strftime(DATETIME_FORMAT),
-        )
-        print(
-            f"saved to db - crowd_count: {crowd_count}, surf_rating: {camera.surf_rating}"
-        )
+            sleep(60)

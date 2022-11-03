8a8c870e3ef599b5eeed95b7f40e116a1255e6f3
Craig Mulligan <git@craigmulligan.com>
2022-11-03 15:27:43 -0600

    sorta

--- lib/app.py
+++ lib/app.py
@@ -14,11 +14,6 @@ app.config.from_object(Config())
 app.teardown_appcontext(DB.tear_down)
 
 
-@app.route("/test")
-def test():
-    return "hi!"
-
-
 @app.route("/")
 def index():
     # get current datetime

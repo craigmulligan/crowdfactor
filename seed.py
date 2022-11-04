47c5cf88887b8ee8f3bfe8b2c1f237a87fe81f7f
Craig Mulligan <git@craigmulligan.com>
2022-11-04 15:24:08 -0600

    much easier to read

---
+++ seed.py
@@ -0,0 +1,13 @@
+from lib.db import DB
+from lib.app import app
+from lib.seed import seed
+
+if __name__ == "__main__":
+    # Running in flask app
+    # context ensures we cleanup db connections
+    # etc.
+    with app.app_context():
+        db = DB.get_db()
+        # ensure db schema
+        db.setup()
+        seed("590927576a2e4300134fbed8")

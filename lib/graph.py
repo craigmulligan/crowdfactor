1a86f70a28898903826e69ecc12be7f27075612e
Craig Mulligan <git@craigmulligan.com>
2022-11-04 17:33:04 -0600

    styles

--- lib/graph.py
+++ lib/graph.py
@@ -3,11 +3,12 @@ from lib.forecast import Forecast
 from lib import utils
 import pygal
 from datetime import datetime
+from pygal.style import CleanStyle
 
 
 class CrowdCount(TypedDict):
     hour: str
-    avg_crowd_count: int
+    avg_crowd_count: float
 
 
 class CrowdPrediction(CrowdCount):
@@ -26,20 +27,20 @@ def prediction_finder(input: List[CrowdPrediction]):
     This function returns the crowd_count for the hour and surf_rating supplied.
     """
 
-    def inner(rating: str, hour: int) -> Optional[int]:
+    def inner(rating: str, hour: int) -> Optional[float]:
         for i in input:
             if int(i["hour"]) == hour and i["surf_rating"] == rating:
-                return i["avg_crowd_count"]
+                return round(i["avg_crowd_count"], 2)
         return None
 
     return inner
 
 
 def reading_finder(input: List[CrowdCount]):
-    def inner(hour: int) -> Optional[int]:
+    def inner(hour: int) -> Optional[float]:
         for i in input:
             if int(i["hour"]) == hour:
-                return i["avg_crowd_count"]
+                return round(i["avg_crowd_count"])
         return None
 
     return inner
@@ -89,10 +90,7 @@ class Graph:
             if prediction:
                 values.append(prediction)
 
-            if local_ts.hour % 2:
-                x_labels.append(f"{local_ts.hour:02}:00")
-            else:
-                x_labels.append(None)
+            x_labels.append({"value": local_ts.hour, "label": f"{local_ts.hour:02}:00"})
 
             label = f"{rating}\n - {local_ts.strftime('%d/%m/%Y')}"
 
@@ -108,7 +106,7 @@ class Graph:
 
             predictions_series.append(
                 {
-                    "value": (prediction, local_ts.hour, local_ts.hour + 1),
+                    "value": (round(prediction, 2), local_ts.hour, local_ts.hour + 1),
                     "style": f"stroke-dasharray: 5, 10; stroke: rgba({r}, {g}, {b}); fill: rgba({r}, {g}, {b}, 0.2);",
                     "label": rating,
                 }
@@ -130,12 +128,20 @@ class Graph:
             _, start, end = f["value"]
             f["value"] = (max_value, start, end)
 
-        chart = pygal.Histogram()
+        chart = pygal.Histogram(
+            x_labels_major_every=2,
+            show_minor_x_labels=False,
+            truncate_label=5,
+            style=CleanStyle,
+        )
+
+        chart.x_value_formatter = lambda x: "%.2f" % x
         chart.show_legend = False
         chart.title = "Crowd factor for today"
         chart.height = 300
+        chart.x_labels = x_labels
 
-        chart.add("Forecast", forecast_series, stroke=False)
+        chart.add("Forecast", forecast_series)
         chart.add(
             "Predicted crowd",
             predictions_series,

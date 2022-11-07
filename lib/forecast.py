56e4633b12b63f29201fb43effb5fbdf57b075e4
Craig Mulligan <git@craigmulligan.com>
2022-11-07 16:27:03 -0600

    add cache for spot_info

--- lib/forecast.py
+++ lib/forecast.py
@@ -20,6 +20,7 @@ class SpotInfo(TypedDict):
     href: str
 
 
+# TODO add an expire arg to shelve_it and cache this.
 def get_latest(spot_id) -> List[Forecast]:
     url = f"https://services.surfline.com/kbyg/spots/forecasts/rating?spotId={spot_id}&days=1&intervalHours=1"
     res = requests.get(url)

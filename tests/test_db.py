42640c0070734a1817d9bd95812fe9198e43d840
Craig Mulligan <git@craigmulligan.com>
2022-11-08 20:38:15 -0600

    fix tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+from lib.utils import epoch_to_string
+
+
 def test_latest_reading(client, db, seed, spot_id, mock_forecast):
     latest = db.latest_reading(spot_id)
     assert latest["surf_rating"] == "FAIR_TO_GOOD"
@@ -21,8 +25,14 @@ def test_predictions(client, db, seed, spot_id):
 def test_readings(client, db, seed, spot_id, seed_window):
 
     _, end = seed_window
-    readings = db.readings(spot_id, end)
+    start = end - timedelta(days=7)
+
+    readings = db.readings(
+        spot_id,
+        epoch_to_string(start.timestamp()),
+        epoch_to_string(end.timestamp()),
+    )
 
     reading = [p for p in readings if int(p["hour"]) == 0][0]
 
-    assert reading["avg_crowd_count"] == 6
+    assert reading["avg_crowd_count"] == 16

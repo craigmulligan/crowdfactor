cd2782edce81c3ded2d5091a8795d978f7724733
Craig Mulligan <git@craigmulligan.com>
2023-01-11 17:54:10 +1300

    cleanup

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -3,7 +3,8 @@ from lib.ml import Model
 def test_first_train(db, seed_training_data):
     # Use all data we have on first train.
     # train on all but 1. And use random 1 to ensure it's the same randomness seed 
-    # on each call. 
+    # on each call.
+
     x_train, x_test, y_train, y_test = Model.get_training_data(random_state=1)
     model = Model.load("test-model")
     model.train(x_train, y_train)

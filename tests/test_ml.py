ca511ae9bcba3788f341366f507d9fdd3b78d030
Craig Mulligan <git@craigmulligan.com>
2023-01-12 12:44:55 +1300

    add logging

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -5,6 +5,7 @@ def test_train_and_persist(db, seed_training_data, app):
     # Use all data we have on first train.
     # train on all but 1. And use random 1 to ensure it's the same randomness seed 
     # on each call.
+    model_name = app.config["MODEL_URL"]
     x_train, x_test, y_train, y_test = Model.get_training_data(random_state=1)
     model = Model.load()
     model.train(x_train, y_train)
@@ -16,4 +17,8 @@ def test_train_and_persist(db, seed_training_data, app):
     assert -8 < score < 8
     
     model.persist()
-    assert os.path.isfile(app.config["MODEL_URL"])
+    model.log(score)
+
+    latest_training_log = db.latest_training_log(model_name)
+    assert latest_training_log["score"] == score
+    assert os.path.isfile(model_name)

8227e16b9875b0ca5d0a5335ac2077cde0f44cf6
Craig Mulligan <git@craigmulligan.com>
2023-01-17 14:52:27 +1300

    add migration lib

---
+++ migrations/2.sql
@@ -0,0 +1,34 @@
+ALTER TABLE crowd_log 
+  ADD wave_height_min REAL;
+
+
+ALTER TABLE crowd_log 
+  ADD wave_height_max REAL;
+
+
+ALTER TABLE crowd_log 
+  ADD weather_temp REAL;
+
+
+ALTER TABLE crowd_log 
+  ADD weather_condition TEXT;
+
+
+ALTER TABLE crowd_log 
+  ADD water_temp_max REAL;
+
+
+ALTER TABLE crowd_log 
+  ADD water_temp_min REAL;
+
+
+ALTER TABLE crowd_log 
+  ADD wind_direction REAL;
+
+
+ALTER TABLE crowd_log 
+  ADD wind_gust REAL;
+
+
+ALTER TABLE crowd_log 
+  ADD wind_speed REAL;
